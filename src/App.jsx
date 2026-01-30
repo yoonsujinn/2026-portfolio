24440acc3a1e4f653228cb6a3f254230d8a90cd0
yoonsujinn <yoonsujin2@naver.com>
2026-01-30 18:33:59 +0900

    111

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <section></section>
+    <section></section>
+    <section></section>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import '../style/header.css'
+
+const Header = () => {
+  return <div className="header">
+    <div className="headerWrap">
+      <div className="headerLeft">
+        윤수진 포트폴리오
+      </div>
+      <div className="headerRight">
+        <ul>
+          <li>Home</li>
+          <li>About me</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+}
+export default Header;
