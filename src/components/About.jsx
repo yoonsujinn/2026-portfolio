70c30353b15bafa873dc56677898e48860af87a2
yoonsujin <yoonsujin2@naver.com>
2026-02-05 23:41:20 +0900

    About 및 AboutBox 컴포넌트 수정: aboutData 추가 및 동적 렌더링 구현; CSS 스타일링 업데이트

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,17 @@
 import "../style/section.css";
 import AboutBox from "./AboutBox";
-const About = ({clsName}) => {
+import {aboutData} from "../data/aboutData.js";
+
+const About = () => {
   return (
     <div className="aboutWrap">
       <h1>about Me</h1>
-      <div className="aboutBoxWrap"></div>
+      <div className="aboutBoxWrap">
+        {aboutData.map((data, idx) => (
+          <AboutBox key={idx} data={data} />
+        ))}
+        {/* <AboutBox /> */}
+      </div>
     </div>
   );
 };
--- src/components/AboutBox.jsx
+++ src/components/AboutBox.jsx
@@ -1,30 +1,26 @@
-const AboutBox = () => {
+import "../style/aboutBox.css";
+
+const AboutBox = ({data}) => {
+  console.log(data, "data~~~");
   return (
     <>
       <div className="aboutBox">
         <div className="aboxBoxTitle">
           <div className="logo"></div>
-          <div className="titleText">Skills</div>
+          <div className="titleText">{data.title}</div>
         </div>
-        <ul className="aboutUL">
-          <li>
-            <span>HTML / CSS</span>
-            <p>
-              웹 표준과 접근성을 고려한 시맨틱 마크업과
-              <br />
-              유지보수를 고려한 스타일링을 합니다.
-            </p>
-          </li>
-          <li>
-            <span>JAVASCIPT</span>
-            <p>ES6+ 문법을 활용해 UI 로직을 구현하고 기능을 구조화합니다.</p>
-          </li>
-          <li>
-            <span>React</span>
-            <p>
-              컴포넌트 구조와 상태 흐름을 이해하고 개인 프로젝트에 적용합니다.
-            </p>
-          </li>
+        <ul className="aboutUl">
+          {data.type === "Skills" &&
+            data.items.map((e, idx) => (
+              <li key={idx}>
+                <span>{e.name}</span>
+                <p>{e.desc}</p>
+              </li>
+            ))}
+          {data.type === "howIwork" &&
+            data.items.map((e, idx) => <li key={idx}> {e.desc}</li>)}
+          {data.type === "cert" &&
+            data.items.map((e, idx) => <li key={idx}> {e.desc}</li>)}
         </ul>
       </div>
     </>
