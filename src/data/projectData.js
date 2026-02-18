be7e8b59561c0ad369c9b9b04df0255dd387622e
yoonsujin <yoonsujin2@naver.com>
2026-02-18 17:32:55 +0900

    sss

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,16 +3,15 @@ export const projectData = [
     type: "MainProject",
     items: [
       {
-        name: "React Todo List",
-        img: "/assets/project/react-todo.png",
+        name: "React To do List",
         members: 1,
-        techStack: ["Vite", "React"],
-        view: "https://yoonsujinn.github.io/react-todo",
+        techStack: ["Vite", "React", "HTML", "CSS"],
         git: "https://github.com/yoonsujinn/react-todo",
+        view: "https://yoonsujinn.github.io/react-todo",
         desc: [
           "리액트를 학습하며 Todo기능을 제작했습니다.",
           "할일 추가, 삭제, 완료 여부 체크 기능이 가능합니다.",
-          "useMemo기능을 활용하여 싷시간으로 todo갯수, 완료, 미완료 여부가 변경됩니다. ",
+          "useMemo기능을 활용하여 실시간으로 todo갯수, 완료, 미완료 여부가 변경됩니다. ",
         ],
       },
     ],
