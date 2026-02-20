9a99760b9c36dc58b4ce99c5261658b174908d6d
yoonsujinn <yoonsujin2@naver.com>
2026-02-20 20:01:41 +0900

    111

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -39,16 +39,32 @@ export const projectData = [
     type: "MainProject",
     items: [
       {
+        projectName: "space-quiz-project",
+        name: "Space Quiz Project",
+        members: 3,
+        techStack: ["HTML", "CSS", "JavaScript"],
+        git: "https://github.com/yoonsujinn/space-quiz-project",
+        view: "https://yoonsujinn.github.io/space-quiz-project/index.html",
+        desc: [
+          "퇴근 후 실력 향상을 목표로 기획·디자인·개발한 팀 사이드 프로젝트입니다.",
+          "우주에서 퀴즈를 푸는 콘셉트의 인터랙티브 교육용 웹 애플리케이션을 구현했습니다",
+        ],
+      },
+    ],
+  },
+  {
+    type: "MainProject",
+    items: [
+      {
         projectName: "greenCard",
-        name: "green Card",
+        name: "Green Card",
         members: 1,
         techStack: ["HTML", "CSS", "JavaScript"],
-        git: "https://github.com/yoonsujinn/react_todo",
-        view: "https://yoonsujinn.github.io/react_todo",
+        git: "https://github.com/yoonsujinn/greenCard",
+        view: "https://yoonsujinn.github.io/greenCard/greenCard.html",
         desc: [
-          "리액트로 구현한 할 일 리스트 페이지 입니다.",
-          "할 일 추가, 삭제, 완료 여부 체크 기능을 사용할 수 있습니다.",
-          "useMemo기능을 활용하여 실시간 todo갯수, 완료, 미완료 여부를 확인할 수 있습니다. ",
+          "그린카드 홈페이지의 디자인과 기능을 클론 코딩하며 CSS 애니메이션과 javascript구현 실력을 높인 프로젝트입니다.",
+          "시맨틱 마크업을 적용해 웹 문서의 구조적 완성도를 높였으며, 웹 접근성을 고려한 키보드 네비게이션 기능을 추가했습니다.",
         ],
       },
     ],
